3383bd75adfaf83721910256257e4b98e2371752
anyhia <gabriela.maricari@gmail.com>
2026-03-03 17:33:12 +0100

    Fix: landing page layout, footer positioning, notification click redirect to /createlog

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,6 +1,6 @@
 import { Container, Button} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faArrowRight, faCalendar, faBrain, faLock} from '@fortawesome/free-solid-svg-icons';
+import {faArrowRight, faMicrophone, faBell, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
 import { Link, useNavigate } from 'react-router-dom'; 
 import Mascot from '../media/images/Mascot.png';
 import './About.css';
@@ -42,22 +42,26 @@ function AboutContent() {
 
             </Container>
             <Button className='get-started-button' as={Link} to="/login">Get started<span><FontAwesomeIcon className='ms-3' icon={faArrowRight} /></span></Button>
-            
+ 
+            <div className='how-to-use-hint'>
+                New here? <Link to="/how-to-use" className='how-to-use-link'>See how it works</Link>
+            </div>
+
             <Container className='show-descriptions'>
                 <div className='description-card blue'>
-                    <div className='description-card-icon icon-blue'><FontAwesomeIcon icon={faCalendar} /></div>
+                    <div className='description-card-icon icon-blue'><FontAwesomeIcon icon={faMicrophone} /></div>
                     <div className='description-card-title'>Write it, say it, forget it</div>
-                    <div className='description-card-text'>No effort, no organizing. HeyDayta holds onto it so you don't have to.</div>
+                    <div className='description-card-text'>No effort, no organizing. HeyDayta holds onto everything so you don't have to.</div>
                 </div>
                 <div className='description-card purple'>
-                    <div className='description-card-icon icon-purple'><FontAwesomeIcon icon={faBrain} /></div>
+                    <div className='description-card-icon icon-purple'><FontAwesomeIcon icon={faBell} /></div>
                     <div className='description-card-title'>Reminders that understand you</div>
                     <div className='description-card-text'>No date pickers, no dropdowns. HeyDayta figures it out.</div>
                 </div>
                 <div className='description-card orange'>
-                    <div className='description-card-icon icon-orange'><FontAwesomeIcon icon={faLock} /></div>
+                    <div className='description-card-icon icon-orange'><FontAwesomeIcon icon={faMagnifyingGlass} /></div>
                     <div className='description-card-title'>Ask. Don't search.</div>
-                    <div className='description-card-text'>When you need something, just ask. Like talking to someone who remembers everything.</div>
+                    <div className='description-card-text'>When you need something from your past, just ask. Like talking to someone who remembers everything.</div>
                 </div>
             </Container>
         
